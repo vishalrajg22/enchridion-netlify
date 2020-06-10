f1d70949b5169d9f2761748b29a919b9395b6773
Vishal Raj <vishalrajg22@gmail.com>
2020-06-10 16:07:35 +0530

    Update ProfilePage.jsx

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,46 +1,46 @@
-import React from "react";
+import React from "react"
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from "classnames"
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from "@material-ui/core/styles/withStyles"
 // @material-ui/icons
-import Camera from "@material-ui/icons/Camera";
-import Favorite from "@material-ui/icons/Favorite";
+import Camera from "@material-ui/icons/Camera"
+import Favorite from "@material-ui/icons/Favorite"
 // React icons
-import { FaLinkedin, FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaLinkedin, FaFacebook, FaInstagram } from "react-icons/fa"
 // core components
-import Header from "components/Header/Header.jsx";
-import Footer from "components/Footer/Footer.jsx";
-import Button from "components/CustomButtons/Button.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import NavPills from "components/NavPills/NavPills.jsx";
-import Parallax from "components/Parallax/Parallax.jsx";
+import Header from "components/Header/Header.jsx"
+import Footer from "components/Footer/Footer.jsx"
+import Button from "components/CustomButtons/Button.jsx"
+import GridContainer from "components/Grid/GridContainer.jsx"
+import GridItem from "components/Grid/GridItem.jsx"
+import HeaderLinks from "components/Header/HeaderLinks.jsx"
+import NavPills from "components/NavPills/NavPills.jsx"
+import Parallax from "components/Parallax/Parallax.jsx"
 
-import profile from "assets/img/faces/geetika.jpg";
+import profile from "assets/img/faces/geetika.jpg"
 
-import studio1 from "assets/img/examples/Panda1.jpg";
-import studio2 from "assets/img/examples/Panda2.jpg";
-import studio3 from "assets/img/examples/Panda3.jpg";
-import studio4 from "assets/img/examples/Panda4.jpg";
-import studio5 from "assets/img/examples/Panda5.jpg";
-import studio6 from "assets/img/examples/Panda6.jpg";
-import studio7 from "assets/img/examples/Panda7.jpg";
-import studio8 from "assets/img/examples/Panda8.jpg";
-import studio9 from "assets/img/examples/Panda9.jpg";
+import studio1 from "assets/img/examples/Panda1.jpg"
+import studio2 from "assets/img/examples/Panda2.jpg"
+import studio3 from "assets/img/examples/Panda3.jpg"
+import studio4 from "assets/img/examples/Panda4.jpg"
+import studio5 from "assets/img/examples/Panda5.jpg"
+import studio6 from "assets/img/examples/Panda6.jpg"
+import studio7 from "assets/img/examples/Panda7.jpg"
+import studio8 from "assets/img/examples/Panda8.jpg"
+import studio9 from "assets/img/examples/Panda9.jpg"
 
-import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx";
+import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx"
 
 class ProfilePage extends React.Component {
   render() {
-    const { classes, ...rest } = this.props;
+    const { classes, ...rest } = this.props
     const imageClasses = classNames(
       classes.imgRaised,
       classes.imgRoundedCircle,
       classes.imgFluid
-    );
-    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+    )
+    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery)
     return (
       <div>
         <Header
@@ -50,7 +50,7 @@ class ProfilePage extends React.Component {
           fixed
           changeColorOnScroll={{
             height: 200,
-            color: "white"
+            color: "white",
           }}
           {...rest}
         />
@@ -67,20 +67,29 @@ class ProfilePage extends React.Component {
                     <div className={classes.name}>
                       <h3 className={classes.title}>Geetika Agarwal</h3>
                       <h6>Content Writer</h6>
-                      <Button justIcon link 
+                      <Button
+                        justIcon
+                        link
                         href="https://www.instagram.com/geetika_agarwal21/"
-                        className={classes.margin5}>
-                        <FaInstagram/>
+                        className={classes.margin5}
+                      >
+                        <FaInstagram />
                       </Button>
-                      <Button justIcon link 
+                      <Button
+                        justIcon
+                        link
                         href="https://www.facebook.com/profile.php?id=100009338507344"
-                        className={classes.margin5}>
-                        <FaFacebook/>
+                        className={classes.margin5}
+                      >
+                        <FaFacebook />
                       </Button>
-                      <Button justIcon link 
+                      <Button
+                        justIcon
+                        link
                         href="https://www.linkedin.com/in/geetika-agarwal-b9502b182/"
-                        className={classes.margin5}>
-                        <FaLinkedin/>
+                        className={classes.margin5}
+                      >
+                        <FaLinkedin />
                       </Button>
                     </div>
                   </div>
@@ -88,14 +97,17 @@ class ProfilePage extends React.Component {
               </GridContainer>
               <div className={classes.description}>
                 <p>
-                  An artist of considerable range, Chet Faker — the name taken
-                  by Melbourne-raised, Brooklyn-based Nick Murphy — writes,
-                  performs and records all of his own music, giving it a warm,
-                  intimate feel with a solid groove structure.{" "}
+                  An artist, writer and a dreamer who enjoys being alone and
+                  exploring the mysteries of life.{" "}
                 </p>
               </div>
               <GridContainer justify="center">
-                <GridItem xs={12} sm={12} md={10} className={classes.navWrapper}>
+                <GridItem
+                  xs={12}
+                  sm={12}
+                  md={10}
+                  className={classes.navWrapper}
+                >
                   <NavPills
                     alignCenter
                     color="primary"
@@ -155,7 +167,7 @@ class ProfilePage extends React.Component {
                               />
                             </GridItem>
                           </GridContainer>
-                        )
+                        ),
                       },
                       {
                         tabButton: "Favorite",
@@ -189,11 +201,11 @@ class ProfilePage extends React.Component {
                                 alt="..."
                                 src={studio4}
                                 className={navImageClasses}
-                              />                      
+                              />
                             </GridItem>
                           </GridContainer>
-                        )
-                      }
+                        ),
+                      },
                     ]}
                   />
                 </GridItem>
@@ -203,8 +215,8 @@ class ProfilePage extends React.Component {
         </div>
         <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default withStyles(profilePageStyle)(ProfilePage);
+export default withStyles(profilePageStyle)(ProfilePage)
